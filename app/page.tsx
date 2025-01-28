ad7287ff64c5b17c575f7cfb9057204380792cce
Sakibur Rahaman <sakib.cse11.cuet@gmail.com>
2025-01-28 13:36:04 +0600

    Implements firebases storage and embedding generation

--- app/page.tsx
+++ app/page.tsx
@@ -56,16 +56,37 @@ export default function Home() {
             </Button>
           </div>
 
-          <div>
-            <div>
+          <div className="relative overflow-hidden pt-16">
+            <div className="mx-auto mx-w-7xl px-6 lg:px-8">
               <Image
                 alt="App Screnshot"
-                src="https://i.imgur.com/ciRTSI.jpeg"
+                src="https://i.imgur.com/12gZ4DS.png"
                 width={2432}
                 height={1442}
                 className="mb-[-0%] rounded-xl shadow2xl ring-1 ring-gray-900/10"
               />
+              <div aria-hidden="true" className="relative">
+                <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+              </div>
             </div>
+          </div>
+
+          <div>
+            <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 
+            lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 pl-8 mt-16">
+              {features.map(feature => (
+                <div key={feature.name} className="relative pl-9">
+                  <dd><b>{feature.name}</b></dd>
+                  <dt className="inline font-semibold text-gray-900">
+                    <feature.icon
+                      aria-hidden="true"
+                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                    />
+                  </dt>
+                  <dd>{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
           </div>
         </div>
       </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui';
 
 export default {
     darkMode: ["class"],
@@ -58,5 +59,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), daisyui],
 } satisfies Config;
