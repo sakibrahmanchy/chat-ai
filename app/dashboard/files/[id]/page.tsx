ff4ae2ab5b7c1442c9de3d848bc8325067266cd6
root <root@Sakib.localdomain>
2025-01-28 15:29:42 +0600

    Building chat functionality

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,6 +1,7 @@
 import PdfView from "@/components/pdf-view";
 import { adminDb } from "@/firebase-admin";
 import { auth } from "@clerk/nextjs/server"
+import Chat from "@/components/chat";
 
 async function ChatToFilePage({ params }: any) {
   const { id } = await params  
@@ -20,7 +21,7 @@ async function ChatToFilePage({ params }: any) {
  console.log(url)
   return (
     <div className="grid lg:grid-cols-5 h-screen overflow-hidden">
-        <div className="col-span-5 lg:col-span-2 overflow-y-auto">Chat</div>
+        <div className="col-span-5 lg:col-span-2 overflow-y-auto"><Chat id={id} /></div>
         <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-scroll pb-20"><PdfView url={url} /></div> 
     </div>
   )
