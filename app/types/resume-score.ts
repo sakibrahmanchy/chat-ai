0265e88c5bf29ebb957950bb58c4c3a848e0a434
Sakibur Rahaman <sakib.cse11.cuet@gmail.com>
2025-02-28 23:00:46 +0600

    List view

---
+++ app/types/resume-score.ts
@@ -0,0 +1,23 @@
+export interface ResumeScore {
+    analysis: Analysis
+    metadata: Metadata
+    skills_score: number
+    overall_score: number
+    education_score: number
+    experience_score: number
+}
+
+export interface Analysis {
+    strengths: string[]
+    weaknesses: string[]
+    matched_skills: any[]
+    missing_skills: string[]
+    overall_feedback: string
+    education_analysis: string
+    experience_analysis: string
+}
+
+export interface Metadata {
+    processed_at: string
+    processing_time: number
+}
